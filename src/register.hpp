41fb8aece4c98158dead58958e0b41fecbbf5323
chiniu <kimpoychin131@gmail.com>
2025-12-17 21:41:01 +0800

    initial-commit

---
+++ src/register.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize(ModuleInitializationLevel p_level);
+void uninitialize(ModuleInitializationLevel p_level);
+
